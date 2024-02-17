ab909f483e72659539bc90be6d1929358b6cb219
di-saldana <dsl42@alu.ua.es>
2024-02-17 16:55:23 +0100

    first commit

---
+++ app/src/main/java/es/ua/eps/raw_filmoteca/LoginActivity.kt
@@ -0,0 +1,11 @@
+package es.ua.eps.raw_filmoteca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
